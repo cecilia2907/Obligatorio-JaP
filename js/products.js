3abf0e615abf31d3725e9a17cd8aa20b22fa25bc
Cecilia Alarcón <68519432+cecilia2907@users.noreply.github.com>
2020-08-05 23:26:16 -0300

    Update products.js
    Cambios realizados para entrega 1 - parte 2

--- js/products.js
+++ js/products.js
@@ -1,6 +1,36 @@
+//Realizar una petición web a una URL donde se encuentra una 
+//colección de productos en formato JSON, 
+//con la información básica (precio, nombre, breve descripción) 
+//respectiva a cada producto.
+
+function LoadJSONProductos () {
+    fetch(PRODUCTS_URL)
+        .then(response => response.json())
+        .then(json => Productos(json))
+        .catch(err => {
+            console.log(err)
+        })
+}
+
+LoadJSONProductos();
+
+
+function Productos(data) {
+    
+    let productos = "";
+
+    for (var i=0; i<data.lenght; i++) {
+        
+        productos += '<div class="producos">' + data.name + data.description + data.cost;
+    
+    }
+};
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    LoadJSONProductos ();
 });
