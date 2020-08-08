75ee7463b31a41c4ab373621f6406c211b57728a
Cecilia Alarcón <68519432+cecilia2907@users.noreply.github.com>
2020-08-08 19:29:20 -0300

    Entrega1

--- js/products.js
+++ js/products.js
@@ -1,40 +1,50 @@
-//Realizar una petición web a una URL donde se encuentra una 
-//colección de productos en formato JSON, 
-//con la información básica (precio, nombre, breve descripción) 
-//respectiva a cada producto, y mostrar el listado en HTML.
-
-//var products = [];
-
-function traerProductos () {
-    fetch(PRODUCTS_URL)
-        .then(response => response.json())
-        .then(productos => {
-            console.log(productos);
-        })
-}
-
-
-//function showProducts(data) {
-  //  console.log(data);
-//}
-
-
-//function products(json) {
-    //let products;
-    //for (var i=0; i<json.lenght; i++) {
-        //productos += '<div class="producos">' + json.name + json.description + json.cost;
-    //}
-    //document.getElementsByClassName("container p-5").innerHTML = productos;
-//};
-
-
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 
-//document.addEventListener("DOMContentLoaded", function (e) {
-    //getJSONData(PRODUCTS_URL).then(data => {
-       // showProducts(data);
-    //})
-//});
+document.addEventListener("DOMContentLoaded", function() {
+    traerProductos();
+})
+
+//Realizar una petición web a una URL donde se encuentra una 
+//colección de productos en formato JSON, 
+//con la información básica (precio, nombre, breve descripción) 
+//respectiva a cada producto, y mostrar el listado en HTML.
+
+var contenido = document.getElementById("lista-de-productos")
+
+function traerProductos () {
+    fetch(PRODUCTS_URL)
+        .then(response => response.json())
+        .then(productos => {
+            //console.log(productos);
+            showProductos(productos);
+        })
+}
+
+function showProducts(productos) {
+    console.log(productos);
+
+    contenido.innerHTML = " "
+
+    for(let i of productos) {
+
+    contenido.innerHTML += `
+    <div class="list-group-item list-group-item-action">
+        <div class="row">
+            <div class="col-3">
+                <img src="${productos.imgSrc}" alt="${productos.description}" class="img-thumbnail">
+            </div>
+            <div class="col">
+                <div class="d-flex w-100 justify-content-between">
+                    <h4 class="mb-1">${productos.name}</h4>
+                    <small class="text-muted">${productos.currency} + ${productos.price}</small>
+                </div>
+                <p class="mb-1">${productos.description}</p>
+            </div>
+        </div>
+    </div>
+    `
+    }
+}
