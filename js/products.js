e2617a060c59285953ad9b57497e9d03008221ac
Cecilia Alarcón <68519432+cecilia2907@users.noreply.github.com>
2020-08-08 20:07:45 -0300

    Entrega1

--- js/products.js
+++ js/products.js
@@ -28,20 +28,20 @@ function showProductos(productos) {
 
     contenido.innerHTML = " "
 
-    for(let i of productos) {
+    for(let i=0; i < productos.length; i++) {
 
     contenido.innerHTML += `
     <div class="list-group-item list-group-item-action">
         <div class="row">
             <div class="col-3">
-                <img src="${productos[i].imgSrc}" alt="${productos[i].description}" class="img-thumbnail">
+                <img src="${productos.imgSrc}" alt="${productos.description}" class="img-thumbnail">
             </div>
             <div class="col">
                 <div class="d-flex w-100 justify-content-between">
-                    <h4 class="mb-1">${productos[i].name}</h4>
-                    <small class="text-muted">${productos[i].currency} ${productos[i].cost}</small>
+                    <h4 class="mb-1">${productos.name}</h4>
+                    <small class="text-muted">${productos.currency} ${productos.cost}</small>
                 </div>
-                <p class="mb-1">${productos[i].description}</p>
+                <p class="mb-1">${productos.description}</p>
             </div>
         </div>
     </div>
