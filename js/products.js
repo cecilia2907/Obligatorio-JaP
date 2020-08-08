1bec483037cbdd9d1b9954669b52a1a807d70c68
Cecilia Alarcón <68519432+cecilia2907@users.noreply.github.com>
2020-08-08 19:02:41 -0300

    Entrega1

--- js/products.js
+++ js/products.js
@@ -1,37 +1,40 @@
 //Realizar una petición web a una URL donde se encuentra una 
 //colección de productos en formato JSON, 
 //con la información básica (precio, nombre, breve descripción) 
-//respectiva a cada producto.
+//respectiva a cada producto, y mostrar el listado en HTML.
 
-function LoadJSONProductos () {
-    const PRODUCTS_URL = "https://japdevdep.github.io/ecommerce-api/product/all.json";
+//var products = [];
+
+function traerProductos () {
     fetch(PRODUCTS_URL)
         .then(response => response.json())
-        .then(json => productos(json))
-        .catch(err => {
-            alert(err)
+        .then(productos => {
+            console.log(productos);
         })
 }
-LoadJSONProductos();
 
 
-function productos(json) {
+//function showProducts(data) {
+  //  console.log(data);
+//}
 
-    let productos;
 
-    for (var i=0; i<json.lenght; i++) {
-        
-        productos += '<div class="producos">' + json.name + json.description + json.cost;
-    }
-
-    document.getElementsByClassName("container p-5").innerHTML = productos;
-};
+//function products(json) {
+    //let products;
+    //for (var i=0; i<json.lenght; i++) {
+        //productos += '<div class="producos">' + json.name + json.description + json.cost;
+    //}
+    //document.getElementsByClassName("container p-5").innerHTML = productos;
+//};
 
 
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
-    LoadJSONProductos ();
-});
+
+//document.addEventListener("DOMContentLoaded", function (e) {
+    //getJSONData(PRODUCTS_URL).then(data => {
+       // showProducts(data);
+    //})
+//});
