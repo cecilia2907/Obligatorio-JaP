15504e4edc4515b602a9ad6187e78795f9e7f746
Cecilia Alarcón <68519432+cecilia2907@users.noreply.github.com>
2020-08-13 22:31:16 -0300

    Validacion y redireccionamiento

--- js/login.js
+++ js/login.js
@@ -1,11 +1,36 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+//document.addEventListener("DOMContentLoaded", function(e){
     
+//});
+
+// Funcioón que se ejecuta una vez que se hace click en el botón
+document.getElementById("boton").addEventListener("click", function() {
+    validar();
 });
 
+// Funcioón que valida si se ingresoó usuario y contraseña
+function validar() {
+    let usuario = document.getElementById("usuario").value;
+    let contrasena = document.getElementById("contrasena").value;
 
-function redirigir() {
-    window.location.assign("index.html");
+    if (usuario == null || usuario == "") {
+        alert("Introduzca su Correo Electrónico");
+        return false;
+    }
+    if (contrasena == null || contrasena == "") {
+        alert("Introduzca su contraseña");
+        return false;
+    }
+    else {  
+        alert("Ingreso Correcto")
+        redirigir();
+    }
 }
+
+// Función que direcciona a la homepage 
+function redirigir() {
+    window.location.href = "index.html";
+}
+
